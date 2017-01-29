cbc27c34ebfb6ecc969f807de7f5fd210e19a2c8
elaye <genard.elie@orange.fr>
2017-01-29 11:32:25 +0000

    Clean and refactor

--- app/MPD.hs
+++ app/MPD.hs
@@ -2,19 +2,29 @@ module MPD
 ( currentSong
 , togglePlayPause
 , fetchPlaylist
+, fetchAllArtists
+, fetchArtistAlbums
+, fetchArtistAlbumSongs
 ) where
 
 import ClassyPrelude
 
 import Network.MPD
-  ( State(..)
+  ( MPD
+  , State(..)
   , Status(..)
   , Song(..)
+  , Metadata(..)
+  , Artist
+  , Album
   , withMPD
   , status
   , play
   , pause
   , playlistInfo
+  , list
+  , search
+  , (=?), (<&>)
   )
 import qualified Network.MPD as M
 
@@ -36,9 +46,24 @@ togglePlayPause = do
         Stopped -> void $ withMPD $ play Nothing
         Paused -> void $ withMPD $ pause False
 
-fetchPlaylist :: IO [Song]
-fetchPlaylist = do
-  res <- withMPD $ playlistInfo Nothing
+mpdReq :: MPD [a] -> IO [a]
+mpdReq req = do
+  res <- withMPD req
   case res of
-    Left err -> print "Err while getting list of songs" >> return []
-    Right songs -> return songs
+    Left err -> print ("MPD error: " ++ (show err)) >> return []
+    Right r -> return r
+
+fetchAllArtists :: IO [M.Value]
+fetchAllArtists = mpdReq $ list Artist Nothing
+
+fetchArtistAlbums :: Artist -> IO (Artist, [Album])
+fetchArtistAlbums artist = do
+  albums <- mpdReq $ list Album (Just artist)
+  return (artist, albums)
+
+fetchArtistAlbumSongs :: Artist -> Album -> IO [Song]
+fetchArtistAlbumSongs artist album = mpdReq req
+  where req = search (Artist =? artist <&> Album =? album)
+
+fetchPlaylist :: IO [Song]
+fetchPlaylist = mpdReq $ playlistInfo Nothing
