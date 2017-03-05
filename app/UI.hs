64de16c3c9be5702e78a65058ce791434fa66f3d
elaye <genard.elie@orange.fr>
2017-03-05 21:03:53 +0000

    Clean

--- app/UI.hs
+++ app/UI.hs
@@ -5,22 +5,12 @@ module UI
 import ClassyPrelude hiding (catch)
 import Library
 import UI.Types
-import Control.Exception.Safe (catch)
 
 import Control.Concurrent.Async (async)
 
-import Control.Monad (void, forever)
 import Lens.Micro.Platform ((^.), (&), (.~), (%~))
 
-import Data.Default (def)
--- import Data.Map.Strict (elemAt)
--- import qualified Data.Set as Set
-import Data.List ((!!))
-import Data.HashMap.Strict (elems)
--- import qualified Data.Vector as V
-
 import qualified UI.Utils as Utils
-import qualified UI.Views.Main as MainView
 import qualified UI.Views.Playlist as PlaylistView
 import qualified UI.Views.Library as LibraryView
 import qualified UI.Views.Help as HelpView
@@ -31,11 +21,9 @@ import qualified UI.Widgets.Notification as Notification
 import qualified UI.Widgets.Library as LibraryWidget
 import UI.Widgets.Filter (isFocusedL)
 
-import Brick.Types (Widget, EventM, Next(..), BrickEvent(..))
-import Brick.Widgets.Core (str)
-import Brick.Widgets.List (list)
+import Brick.Types (Widget, EventM, Next, BrickEvent(..))
 import Brick.AttrMap (AttrMap, attrMap)
-import Brick.BChan (BChan(..), newBChan, writeBChan)
+import Brick.BChan (BChan, newBChan, writeBChan)
 import Brick.Util (clamp)
 import qualified Brick.Main as M
 
@@ -58,8 +46,7 @@ appEvent :: AppState UIName -> BrickEvent UIName MPDEvent -> NextState UIName
 appEvent state event = case event of
   AppEvent MPDPlaylistEvent -> updatePlaylist state
   AppEvent MPDStatusEvent -> updateStatus state
-  vtyEvent -> case (state^.filterStateL.isFocusedL) of
-    -- True -> liftM catch (handleViewEvent state event) (\e -> (liftIO $ print "Exception") >> M.continue state)
+  vtyEvent -> case state^.filterStateL.isFocusedL of
     True -> handleViewEvent state vtyEvent
     False -> case vtyEvent of
       VtyEvent (Vty.EvKey (Vty.KChar '?') []) -> M.continue $ state & helpActive %~ not
@@ -78,48 +65,36 @@ handleViewEvent state event = case state^.activeView of
 
 updatePlaylist :: AppState UIName -> NextState UIName
 updatePlaylist state = do
-  songs <- liftIO $ fetchPlaylist
-  -- let playlistWidget = list (UIName "playlist") (fromList songs) 1
-  M.continue $ state & playlistStateL %~ (Playlist.update songs)
+  songs <- liftIO fetchPlaylist
+  M.continue $ state & playlistStateL %~ Playlist.update songs
 
 updateStatus :: AppState n -> NextState n
 updateStatus state = do
-  st <- liftIO $ fetchStatus
+  st <- liftIO fetchStatus
   M.continue $ state & status .~ st
 
 increaseVolume :: AppState n -> IO ()
-increaseVolume state = changeVolume (+3) state
+increaseVolume = changeVolume (+3)
 
 decreaseVolume :: AppState n -> IO ()
-decreaseVolume state = changeVolume (\i -> i-3) state
+decreaseVolume = changeVolume (\i -> i-3) 
 
 changeVolume :: (Int -> Int) -> AppState n -> IO ()
 changeVolume f state = case volume of
   Nothing -> return ()
   Just v -> setVolume $ clamp (f v) 0 100
-  where volume = (stVolume (state^.status))
+  where volume = stVolume (state^.status)
 
 initialState :: [Song] -> Library -> Status -> AppState UIName
-initialState playlist library status = AppState
-  -- { _playlist = list (UIName "playlist") (fromList playlist) 1
+initialState playlist library st = AppState
   { _playlistStateL = Playlist.mkState (UIName "playlist") playlist
   , _filterStateL = Filter.mkState (UIName "filter")
   , _activeView = LibraryView
   , _libraryStateL = LibraryWidget.mkState (UIName "artists") (UIName "albums") (UIName "songs") library
-  -- , _library = library
-  -- , _filteredLibrary = library
-  -- , _libraryArtists = list (UIName "artists") (fromList (keys artists)) 1
-  -- , _libraryAlbums = list (UIName "albums") firstArtistAlbums 1
-  -- , _librarySongs = list (UIName "songs") V.empty 1
-  -- , _libraryActiveColumn = ArtistsColumn
-  -- , _libraryMode = ArtistsAlbumsSongsMode
-  , _status = status
+  , _status = st
   , _helpActive = False
   , _notificationState = Notification.mkState
   }
-  -- where
-  --   artists = library^.artistsL
-  --   firstArtistAlbums = V.fromList . Set.toAscList $ snd $ elemAt 0 artists
 
 attributesMap :: AttrMap
 attributesMap = attrMap Vty.defAttr $ concat
@@ -150,15 +125,15 @@ toEvent _ = MPDUnknownEvent
 mpdLoop :: BChan MPDEvent -> IO ()
 mpdLoop chan = forever $ do
   subsChanges <- mpdReq $ idle [UpdateS, PlaylistS, OptionsS, PlayerS, MixerS]
-  mapM_ (writeBChan chan) (map toEvent subsChanges)
+  mapM_ (writeBChan chan.toEvent) subsChanges
 
 start :: IO ()
 start = do
   playlist <- fetchPlaylist
   library <- fetchLibrary
-  status <- fetchStatus
+  st <- fetchStatus
   mpdEventChan <- newBChan 10
-  let initState = initialState playlist library status
-  async $ mpdLoop mpdEventChan
+  let initState = initialState playlist library st
+  _ <- async $ mpdLoop mpdEventChan
   void $ M.customMain (Vty.mkVty defaultConfig)
                     (Just mpdEventChan) app initState
--- app/UI/Views/Playlist.hs
+++ app/UI/Views/Playlist.hs
@@ -4,35 +4,15 @@ module UI.Views.Playlist
 ) where
 
 import ClassyPrelude hiding ((<>))
-import Data.Monoid ((<>))
 
-import Brick.Types (Widget(..), EventM, Next)
+import Brick.Types (BrickEvent(..), Widget(..), EventM, Next)
 import qualified Graphics.Vty as V
 import Brick.Main (continue, halt)
-import Brick.Widgets.List
-  ( listSelectedL
-  , listRemove
-  , listMoveUp
-  , listMoveDown
-  , listElementsL
-  , handleListEvent
-  , List
-  )
+import Lens.Micro.Platform ((^.), (&), (.~))
 
-import Data.Vector ((!))
-import Data.List ((!!))
-import Data.HashMap.Strict (elems)
-import Lens.Micro.Platform ((^.), (%~), (&), (.~))
-
-import Brick.Types (BrickEvent(..))
-import Brick.Widgets.List (list)
-import Brick.Widgets.Core ((<+>))
-
-import UI.Types (AppState, ViewType(..), playlistStateL, activeView)
+import UI.Types (AppState, playlistStateL)
 import qualified UI.Views.Main as Main
 import qualified UI.Widgets.Playlist as Playlist
-
-import Network.MPD (withMPD, Song(..), Id(..), playId)
 
 type NextState n = EventM n (Next (AppState n))
 
@@ -40,45 +20,10 @@ draw :: (Show n, Ord n) => AppState n -> [Widget n]
 draw state = Main.draw state playlst
   where playlst = Playlist.mkWidget (state^.playlistStateL)
 
--- event :: (Ord n) => AppState n -> BrickEvent n e -> NextState n
--- event state (VtyEvent e) = case e of
---     (V.EvKey (V.KChar 'j') []) -> next state
---     (V.EvKey (V.KChar 'k') []) -> previous state
---     (V.EvKey V.KEnter []) -> play state
---     (V.EvKey (V.KChar 'q') []) -> halt state
---     ev -> listEvent ev state
--- event state _ = continue state
-
 event :: (Ord n) => AppState n -> BrickEvent n e -> NextState n
 event state (VtyEvent e) = case e of
-    -- (V.EvKey (V.KChar 'j') []) -> next state
-    -- (V.EvKey (V.KChar 'k') []) -> previous state
-    -- (V.EvKey V.KEnter []) -> play state
     (V.EvKey (V.KChar 'q') []) -> halt state
-    -- ev -> listEvent ev state
     ev -> do
       newPlaylistState <- Playlist.handleEvent ev (state^.playlistStateL)
       continue $ state & playlistStateL .~ newPlaylistState
-    -- _ -> continue state
 event state _ = continue state
-
--- next :: AppState n -> NextState n
--- next state = continue $ state & playlist %~ listMoveDown
-
--- previous :: AppState n -> NextState n
--- previous state = continue $ state & playlist %~ listMoveUp
-
--- play :: AppState n -> NextState n
--- play state = case (state^.playlist.listSelectedL) of
---   Nothing -> continue state
---   Just i -> do
---     let selectedSong = (state^.playlist.listElementsL) ! i
---     case (sgId selectedSong) of
---       Nothing -> continue state
---       Just id -> do
---         _ <- liftIO $ withMPD $ playId id
---         continue state
-
--- listEvent :: (Ord n) => V.Event -> AppState n -> NextState n
--- listEvent event state = continue =<< (\m -> state & playlist .~ m) <$> handleListEvent event (state^.playlist)
-
