0cc4b11a38687dbabcbb41d408afcb5bfb11da76
elaye <genard.elie@orange.fr>
2017-04-01 10:34:16 +0100

    Add keybindings to help view

--- app/UI/Views/Help.hs
+++ app/UI/Views/Help.hs
@@ -2,13 +2,36 @@ module UI.Views.Help
 ( draw
 ) where
 
+import ClassyPrelude
 import Brick.Types (Widget)
-import Brick.Widgets.Core (str)
+import Brick.Widgets.Core (str, vBox)
 
 draw :: [Widget n]
-draw = [ str "help"
-       , str "j    move down"
-       , str "k    move up"
-       , str "q    quit app or close current view"
-       , str "?    toggle help"
-       ]
+draw = [vBox $ str <$> [ " Help"
+       , " "
+       , " General"
+       , " "
+       , "   j      move down"
+       , "   k      move up"
+       , "   h      move left"
+       , "   l      move right"
+       , " "
+       , "   c      clear playlist"
+       , "   p      play/pause"
+       , "   -      decrease volume"
+       , "   +      increase volume"
+       , " "
+       , "   1      show playlist"
+       , "   2      show library"
+       , " "
+       , "   q      quit app or close current view"
+       , "   ?      toggle help"
+       , " "
+       , " Library"
+       , " "
+       , "   a        add to playlist"
+       , "   <Enter>  add to playlist and play"
+       , "   t        toggle between artist/albums and albums view"
+       , "   /        filter"
+       , "   <Esc>    disable filter if active"
+       ]]
